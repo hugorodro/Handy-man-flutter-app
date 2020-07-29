be4a8897cb0e96039f066c5a02c866360511334e
Hugo Rodriguez <44950015+hugorodro@users.noreply.github.com>
2020-07-29 14:02:25 -0400

    Several Changes

--- lunacon_production_restapi/lunaconAPI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LunaconapiConfig(AppConfig):
-    name = 'lunaconAPI'
--- lunacon_production_restapi/manage.py
+++ lunacon_production_restapi/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunaconWeb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lunacon_production_django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
